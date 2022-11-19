751f05cd58105948bd1da0972ddbcf508247e2b5
Denis <d.cochew@yandex.ru>
2022-11-19 12:22:18 +0700

    Решение

--- Program/Program.cs
+++ Program/Program.cs
@@ -10,3 +10,29 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
+Console.Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    arrayStrings[i] = element;
+}
+
+string[] arrayFinish = new string[size];
+int max = 3;
+int sum = 0;
+
+for (int j = 0; j < size; j++)
+{
+    if (arrayStrings[j].Length <= max)
+    {
+        arrayFinish[sum] = arrayStrings[j];
+        sum++;
+    }
+}
+
+Console.WriteLine();
+PrintArray(arrayFinish);
